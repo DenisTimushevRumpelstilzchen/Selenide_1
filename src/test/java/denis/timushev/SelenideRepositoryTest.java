53e269108bc454efb4537f08a9e4114a565dab9e
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-05-02 21:27:05 +0300

    add SelenideRepositoryTest

--- src/test/java/denis/timushev/SelenideRepositoryTest.java
+++ src/test/java/denis/timushev/SelenideRepositoryTest.java
@@ -9,10 +9,9 @@ public class SelenideRepositoryTest {
 
     @Test
     void shouldFindSelenideAsFirstRepository(){
-        open("https://github.com");  // открыть страницу github.com
-        $("[data-test-selector=nav-search-input]")
-                .setValue("selenide").pressEnter();  // ввести в поле поиска selenide и нажать Enter
-        $$("ul.repo-list li").first().$("a").click();  // нажимаем на линк от первого результата поиска
-        $("h2").shouldHave(text("selenide / selenide"));  // check: в заголовке встречается selenide/selenide
+        open("https://github.com");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("h2").shouldHave(text("selenide / selenide"));
     }
 }
