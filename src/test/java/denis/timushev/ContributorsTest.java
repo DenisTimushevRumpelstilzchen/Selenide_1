2b221d6a71003f66181b0fe4b1966f28d6125c33
Your NameDenis <tdv14011984@gmail.com>
2022-05-03 11:17:08 +0300

    add ContributorsTest

--- src/test/java/denis/timushev/ContributorsTest.java
+++ src/test/java/denis/timushev/ContributorsTest.java
@@ -14,13 +14,9 @@ public class ContributorsTest {
     void solntsevShouldBeFirstContributor(){
 
         Configuration.browserSize="1200x400";
-        // open repository page
         open("https://github.com/selenide/selenide");
-        // bring mouse over the first avatar on contributors tab
         $(".Layout-sidebar").$(byText("Contributors")).ancestor("div")
                 .$("ul li").hover();
-
-        // check: popup is showing Andres Solntsev
         $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
 
     }
