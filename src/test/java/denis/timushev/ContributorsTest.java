5a8602f4c83229a7caa22e2755d21abb8f970b99
Your NameDenis <tdv14011984@gmail.com>
2022-05-03 11:13:56 +0300

    add ContributorsTest

---
+++ src/test/java/denis/timushev/ContributorsTest.java
@@ -0,0 +1,27 @@
+package denis.timushev;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContributorsTest {
+
+    @Test
+    void solntsevShouldBeFirstContributor(){
+
+        Configuration.browserSize="1200x400";
+        // open repository page
+        open("https://github.com/selenide/selenide");
+        // bring mouse over the first avatar on contributors tab
+        $(".Layout-sidebar").$(byText("Contributors")).ancestor("div")
+                .$("ul li").hover();
+
+        // check: popup is showing Andres Solntsev
+        $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
+    }
+}
